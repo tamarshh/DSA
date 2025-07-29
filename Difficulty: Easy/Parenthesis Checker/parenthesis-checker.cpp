55c41fbdeba743b163a7b5cc2d0f84c2ef7bdd7e
Tamarsh Shekhar Singh <161630688+tamarshh@users.noreply.github.com>
2025-07-29 22:12:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -0,0 +1,41 @@
+
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        // code here
+         if(s.length()<=1){
+            return false;
+        }
+        stack<char> stack;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='[' || s[i]=='{' || s[i]=='('){
+                stack.push(s[i]);
+            }
+            else if(stack.empty()){
+                return false;
+            }
+            else{
+                if(s[i]==')'){
+                    if(stack.top()!='('){
+                        return false;
+                    }
+                }
+                if(s[i]=='}'){
+                    if(stack.top()!='{'){
+                        return false;
+                    }
+                }
+                if(s[i]==']'){
+                    if(stack.top()!='['){
+                        return false;
+                    }
+                }
+                stack.pop();
+            }
+        }
+        if (stack.empty()){
+            return true;
+        }
+        return false;
+    }
+};
