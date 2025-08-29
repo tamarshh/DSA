3deabf4060be1c345daaa3c437e948a2bbe004fd
Tamarsh Shekhar Singh <161630688+tamarshh@users.noreply.github.com>
2025-08-29 13:14:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (40.72%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
+int calcPalindrome(int num){
+    int temp=num;
+    long long int rev=0;
+    while(temp!=0){
+        int ld=temp%10;
+         rev=rev*10+ld;
+        temp/=10;
+    }
+return rev;
+
+}
     bool isPalindrome(int x) {
-        if(x<0){
-            return false;
-        }
-        long rev=0;
-        long dup=x;
-        while(x!=0)
-        {
-           long ld=x%10;
-       
-           rev=rev*10+ ld;
-           x=x/10;
-        }
-       return (rev==dup);
+        if(x<0) return false;
+        int rev=calcPalindrome(x);
+        if(rev==x) return true;
+
+        return false;
+
+        
     }
 };
