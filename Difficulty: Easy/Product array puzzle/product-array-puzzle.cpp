066d9116d71d0c5c36cd2fac3efc781bdbdb0338
Tamarsh Shekhar Singh <tamarshss2003@gmail.com>
2025-09-02 23:09:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
@@ -0,0 +1,34 @@
+// User function template for C++
+
+class Solution {
+  public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        // code here
+        int n=nums.size();
+        vector<int>prefix(n,1);
+        vector<int>suffix(n,1);
+        vector<int> ans(n);
+
+        prefix[0]=1;
+        suffix[n-1]=1;
+
+        // prefix product 
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]*nums[i-1];
+        }
+
+        // suffix product
+        for(int i=n-2;i>=0;i--){
+            suffix[i]=suffix[i+1]*nums[i+1];
+        } 
+
+        // calculating the ans
+
+        for(int i=0;i<n;i++){
+            ans[i]=suffix[i]*prefix[i];
+
+        }
+        return ans;
+
+    }
+};
