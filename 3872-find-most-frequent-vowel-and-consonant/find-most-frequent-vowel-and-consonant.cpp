9e2d8777bd5a3fbc4869b014a18144557f197fe9
Tamarsh Shekhar Singh <tamarshss2003@gmail.com>
2025-09-13 17:16:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (30.82%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -1,39 +1,60 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
-         unordered_map<char, int> mp1;
-        unordered_map<char, int> mp2;
-        string vowels="aeiou";
+    //      unordered_map<char, int> mp1;
+    //     unordered_map<char, int> mp2;
+    //     string vowels="aeiou";
 
-        for( auto ch :s){
-            if(vowels.find(ch) != string::npos){
-                mp1[ch]++;
-            }
-            else {
-                 mp2[ch]++;
+    //     for( auto ch :s){
+    //         if(vowels.find(ch) != string::npos){
+    //             mp1[ch]++;
+    //         }
+    //         else {
+    //              mp2[ch]++;
 
-            }
-        }
-       int maxi1 = 0;
-        int maxi2 = 0;
+    //         }
+    //     }
+    //    int maxi1 = 0;
+    //     int maxi2 = 0;
 
-        for( auto it :mp1){
-            if(it.second>maxi1){
-                maxi1=it.second;
-            }
-        }
-        for( auto it :mp2){
-            if(it.second>maxi2){
-                maxi2=it.second;
-            }
-        }
+    //     for( auto it :mp1){
+    //         if(it.second>maxi1){
+    //             maxi1=it.second;
+    //         }
+    //     }
+    //     for( auto it :mp2){
+    //         if(it.second>maxi2){
+    //             maxi2=it.second;
+    //         }
+    //     }
 
-        return maxi1+maxi2;
+    //     return maxi1+maxi2;
        
 
 
+   
+        unordered_map<char, int> freq;
+        string vowels = "aeiou";
 
+        int maxi1 = 0; // max vowel frequency
+        int maxi2 = 0; // max consonant frequency
 
-        
+        for (char ch : s) {
+            freq[ch]++;  // count frequency of character
+
+            if (vowels.find(ch) != string::npos) {
+                maxi1 = max(maxi1, freq[ch]); // update vowel max
+            } else {
+                maxi2 = max(maxi2, freq[ch]); // update consonant max
+            }
+        }
+
+        return maxi1 + maxi2;
     }
 };
+
+
+
+
+
+  
